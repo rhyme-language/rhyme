637f39f5821329c95bd2a2c0c1ed6ef6999ececc
EightyVice <8yvice@gmail.com>
2024-03-31 22:56:31 +0200

    Initial documentation and specification

---
+++ src/Rhyme.Test/Compiler/Compilation/CompilerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhyme;
+
+namespace Rhyme.Test.Compiler.Compilation
+{
+    [TestClass]
+    public class CompilerTest
+    {
+        [TestMethod]
+        public void BulkTestCompile()
+        {
+
+        }
+    }
+}
--- src/Rhyme/Program.cs
+++ src/Rhyme/Program.cs
@@ -25,29 +25,3 @@ compiler.Parameters = new CompilerParameters
 };
 
 compiler.CompileFromFiles(console_options.SourceFiles.Select(f => f.FullName).ToArray());
-
-/*
-string[] lines = source.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-
-void ReportError(PassError error)
-{
-    var accum_start = 0;
-    for (int i = 1; i < error.Line; i++)
-    {
-        accum_start += lines[i - 1].Length;
-
-        // Because it counts \n\r (Needs investigation)
-        accum_start += 2;
-    }
-    var relative_start = error.Start - accum_start;
-
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine($"ERR: [{error.Line}]: {error.Message}: ");
-    Console.ResetColor();
-    Console.WriteLine(lines[error.Line - 1]);
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine(new string(' ', relative_start) + '^' + new string('~', error.Length - 1));
-    Console.ResetColor();
-    Console.WriteLine();
-}
-*/
